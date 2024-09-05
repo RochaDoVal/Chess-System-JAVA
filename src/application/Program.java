cb85453e34c23b54fc2cfa46f876a44ba433a8c8
pedro vitor <pedrovitorrochang@gmail.com>
2024-09-05 14:48:18 -0300

    Terminal git

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,7 +4,7 @@ import Chess.ChessMatch;
 
 public class Program {
 	public static void main (String[] args) {
-
+//abra a pasta BIN e abra o terminal git, e digite -> java application/Program
 
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
